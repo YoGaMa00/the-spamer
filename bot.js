cb645ffe52d327999acdd147fd6398de6a121569
YoGaMa00 <53354020+YoGaMa00@users.noreply.github.com>
2019-07-28 02:59:29 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("NTc5MDcyOTIwNzU2OTQ0OTEz.XTztmA.d_VAoplAgkHIbN_AQGlZ3dV9Yp0")
+setInterval(function() {
+channel.send(انا هبقى مليونير);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
